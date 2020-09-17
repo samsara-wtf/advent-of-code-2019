a5de415f9c772cc19497129c4acf0e854ad9761a
seth <samsara@hey.com>
2020-09-17 01:31:52 +0000

    messy, but solved part 2

--- src/day01.rs
+++ src/day01.rs
@@ -14,7 +14,9 @@ fn read_lines() -> std::string::String {
 }
 
 fn convert_to_numbers(data: std::string::String) -> Vec<i32> {
-    data.lines().map(|line| i32::from_str(line).unwrap()).collect()
+    data.lines()
+        .map(|line| i32::from_str(line).unwrap())
+        .collect()
 }
 
 fn fuel_for_mass(kg: i32) -> i32 {
@@ -24,8 +26,32 @@ fn fuel_for_mass(kg: i32) -> i32 {
 fn solve() -> i32 {
     convert_to_numbers(read_lines())
         .iter()
-        .map( |mass| fuel_for_mass(*mass) )
+        .map(|mass| fuel_for_mass(*mass))
         .sum()
+}
+
+fn solve2() -> i32 {
+    convert_to_numbers(read_lines())
+        .iter()
+        .map(|mass| fuel_for_mass_and_fuel(*mass))
+        .sum()
+}
+
+fn fuel_for_mass_and_fuel(kg: i32) -> i32 {
+    let mut to_move = kg;
+    let mut total_fuel = 0;
+
+    loop {
+        let fuel = fuel_for_mass(to_move);
+
+        if fuel > 0 {
+            total_fuel += fuel;
+            to_move = fuel;
+        }
+        else {
+            break total_fuel
+        }
+    }
 }
 
 #[cfg(test)]
@@ -33,8 +59,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_fuel_for_fuel() {
+        assert_eq!(2, fuel_for_mass_and_fuel(14));
+        assert_eq!(2, fuel_for_mass_and_fuel(14));
+        assert_eq!(966, fuel_for_mass_and_fuel(1969));
+        assert_eq!(50346, fuel_for_mass_and_fuel(100756));
+    }
+
+    #[test]
     fn test_solution() {
-        assert_eq!(42, solve());
+        // assert_eq!(42, solve());
+        assert_eq!(42, solve2());
     }
 
     #[test]
