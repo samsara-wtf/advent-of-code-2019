665f12c045aa89482958afce48f5bce1e5c2a790
seth <samsara@hey.com>
2020-09-17 00:40:37 +0000

    Solved day 01, part one

--- src/day01.rs
+++ src/day01.rs
@@ -17,9 +17,25 @@ fn convert_to_numbers(data: std::string::String) -> Vec<i32> {
     data.lines().map(|line| i32::from_str(line).unwrap()).collect()
 }
 
+fn fuel_for_mass(kg: i32) -> i32 {
+    kg / 3 - 2
+}
+
+fn solve() -> i32 {
+    convert_to_numbers(read_lines())
+        .iter()
+        .map( |mass| fuel_for_mass(*mass) )
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_solution() {
+        assert_eq!(42, solve());
+    }
 
     #[test]
     fn test_convert_to_numbers() {
@@ -28,10 +44,11 @@ mod tests {
         assert_eq!(97852, numbers[numbers.len() - 1])
     }
 
-    // #[test]
-    // fn test_read_lines() {
-    //     let a = read_lines();
-    //     let b = read_lines();
-    //     assert_eq!(a, b);
-    // }
+    #[test]
+    fn test_fuel_for_mass() {
+        assert_eq!(2, fuel_for_mass(12));
+        assert_eq!(2, fuel_for_mass(14));
+        assert_eq!(654, fuel_for_mass(1969));
+        assert_eq!(33583, fuel_for_mass(100756));
+    }
 }
